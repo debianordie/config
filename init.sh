b45841a2ab4c8e5ea256363b13d066e9f19a5ae0
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 20:57:04 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sudo apt-get -qqy update
 sudo apt-get -qqy install git
 
 REPO=$(mktemp --directory)
-git clone --quiet https://github.com/debianordie/testing.git ${REPO}
+git clone -q https://github.com/debianordie/testing.git ${REPO}
 
 sh ${REPO}/config.sh
