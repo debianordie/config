60d5ccf187230b8701099a04eed284b1017d5bae
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 15:23:40 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-echo "ok"
+export DEBIAN_FRONTEND=noninteractive
+
+alias package='sudo apt-get --quiet --quiet --yes'
+alias service='sudo systemctl --quiet'
+
+pkill --full /usr/libexec/gnome-initial-setup
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'es')]"
+gsettings set org.gnome.system.location enabled false
+gsettings set org.gnome.shell favorite-apps "['terminator.desktop', 'geany.desktop', 'org.gnome.Nautilus.desktop', 'zim.desktop', 'firefox-esr.desktop', 'thunderbird.desktop', 'telegramdesktop.desktop', 'keepassx.desktop', 'virt-manager.desktop']"
+
+gsettings set org.gnome.desktop.session idle-delay 0
+
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+
+gsettings set org.gnome.desktop.interface clock-format '24h'
+sudo ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+
+while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do
+    sleep 1
+done
+
+package update
+package upgrade
+package install telegram-desktop keepassx zim virt-manager terminator vim git geany-plugin-treebrowser
+package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
