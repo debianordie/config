3425d572d66d3a5e1a62e29359ebc355d3a3540b
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 15:33:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -24,5 +24,15 @@ done
 
 package update
 package upgrade
+package install sway swaybar
 package install telegram-desktop keepassx zim virt-manager terminator vim git geany-plugin-treebrowser
 package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
+
+echo 'export MOZ_ENABLE_WAYLAND=1 # for firefox
+export XDG_SESSION_TYPE=wayland 
+export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
+# automatically login into sway
+if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+  exec sway
+fi' > ~/.profile
