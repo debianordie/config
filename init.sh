a9763fe2c8e07ff6fed7d008ea4c43219d41dac7
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:28:34 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,15 +5,16 @@ export DEBIAN_FRONTEND=noninteractive
 export BASEDIR=$(dirname "$0")
 
 alias package='sudo apt-get --quiet --quiet --yes'
+alias cd_temp_dir="cd $(mktemp --directory)"
 
 package update
 package install git
 
-REPO=$(mktemp --directory)
-git clone -q https://github.com/debianordie/config.git ${REPO}
-
 mkdir -p ~/.config/dod
-cp -a ${REPO}/imports.sh ~/.config/dod/00_init.sh
+
+cd_temp_dir
+git clone -q https://github.com/debianordie/config.git .
+cp -a ./imports.sh ~/.config/dod/00_init.sh
 
 echo 'for FILE in $(ls -1 ~/.config/dod); do
   . ~/.config/dod/$FILE
@@ -22,7 +23,7 @@ done' > ~/.dod
 . ~/.dod
 
 if [ ! -z CONFIG ]; then
-  REPO=$(mktemp --directory)
-  git clone -q https://github.com/debianordie/${CONFIG}.git ${REPO}
-  sh ${REPO}/config.sh
+  cd_temp_dir
+  git clone -q https://github.com/debianordie/${CONFIG}.git .
+  sh ./config.sh
 fi
