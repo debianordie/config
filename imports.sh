2cec5344c073c58865da165ac945efc383212c46
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 23:51:07 +0000

    Update imports.sh

--- imports.sh
+++ imports.sh
@@ -6,3 +6,26 @@ export BASEDIR=$(dirname "$0")
 
 alias package='sudo apt-get --quiet --quiet --yes'
 alias service='sudo systemctl --quiet'
+
+ensure_dod_module () {
+  if curl -sf "https://github.com/debianordie/${1}" >/dev/null; then
+    if [ -d "~/.config/dod/${1}" ]; then
+      git pull -q
+    else
+      git clone -q https://github.com/debianordie/${1}.git ~/.config/dod/${CONFIG}
+    fi
+  else
+    _error "module <${1}> does not exist"
+  fi
+}
+
+ensure_dod_module () {
+  if [ ! -d "~/.config/dod/${1}" ]; then
+    get_dod_module "${1}"
+  fi
+  if [ -f "~/.config/dod/${1}/config.sh" ]; then
+    . "~/.config/dod/${1}/config.sh"
+  else
+    _error "~/.config/dod/${1} has not <config.sh> file"
+  fi
+}
