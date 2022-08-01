67cdcffaac4ba4d3c71a6a2e9f6773ee510e8863
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:04:09 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-sudo apt-get -qqy update
-sudo apt-get -qqy install git
+BASEDIR=$(dirname "$0")
+
+mkdir -p ~/.dod
+
+cp -a ${BASEDIR}/imports.sh ~/.dod/00_init.sh
+
+echo 'for FILE in $(ls -1 ~/.dod); do
+  source ~/.dod/$FILE
+done' > ~/.bash_aliases
+
+source ~/.bash_aliases
+
+package install git
 
 REPO=$(mktemp --directory)
 git clone -q https://github.com/debianordie/${CONFIG}.git ${REPO}
-
 sh ${REPO}/config.sh
