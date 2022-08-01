d0a24cef0026c51c2e2ca0e17d1fd20f51ddd818
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:41:45 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,21 +2,18 @@
 set -Eex
 
 alias package='DEBIAN_FRONTEND=noninteractive sudo apt-get --quiet --quiet --yes'
-
 package update
 package install git
 
-mkdir -p ~/.config/dod
-
 cd $(mktemp --directory)
 git clone -q https://github.com/debianordie/config.git .
+
+mkdir -p ~/.config/dod
 cp -a ./imports.sh ~/.config/dod/00_init.sh
 
 echo 'for FILE in $(ls -1 ~/.config/dod); do
   . ~/.config/dod/$FILE
 done' > ~/.dod
-
-. ~/.dod
 
 if [ ! -z CONFIG ]; then
   cd $(mktemp --directory)
