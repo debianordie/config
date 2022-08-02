2065e239a321ab263411a8e4bbc93a297659925b
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-02 00:53:10 +0000

    Update imports.sh

--- imports.sh
+++ imports.sh
@@ -9,22 +9,23 @@ alias service='sudo systemctl --quiet'
 
 ensure_dod_module () {
   if curl -sf "https://github.com/debianordie/${1}" >/dev/null; then
-    if [ -d "~/.config/dod/${1}" ]; then
-      git -C ~/.config/dod/config pull -q "~/.config/dod/${1}"
+    if [ -d ~/.config/dod/${1} ]; then
+      #git -C ~/.config/dod/config pull -q "~/.config/dod/${1}"
+      echo
     else
-      git clone -q https://github.com/debianordie/${1}.git ~/.config/dod/${CONFIG}
+      git clone -q https://github.com/debianordie/${1}.git ~/.config/dod/${1}
     fi
   else
     _error "module <${1}> does not exist"
   fi
 }
 
-ensure_dod_module () {
-  if [ ! -d "~/.config/dod/${1}" ]; then
-    get_dod_module "${1}"
+run_dod_module () {
+  if [ ! -d ~/.config/dod/${1} ]; then
+    ensure_dod_module "${1}"
   fi
-  if [ -f "~/.config/dod/${1}/config.sh" ]; then
-    . "~/.config/dod/${1}/config.sh"
+  if [ -f ~/.config/dod/${1}/config.sh ]; then
+    . ~/.config/dod/${1}/config.sh
   else
     _error "~/.config/dod/${1} has not <config.sh> file"
   fi
